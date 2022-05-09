63dc1ffd3cc56d006358983b883b3e6f3fd1c78c
holger <h.trahe@relux.com>
2022-05-09 14:08:25 +0200

    [ADD] added some useful impl for the GldfProduct struct., some more README.md

--- src/tests.rs
+++ src/tests.rs
@@ -1,25 +1,19 @@
 use gldf::GldfProduct;
 use crate::gldf;
 
+
 #[test]
-fn parsing_gldf_namespace() {
-    use std::fs;
+fn parsing_gldf_container() {
     use yaserde::de::from_str;
     use serde_json::from_str as serde_from_str;
-
-    let filename = "./tests/data/product.xml";
-    let content = fs::read_to_string(filename).expect("something went wrong reading the file");
-
-    let loaded: GldfProduct = from_str(&content).unwrap();
+    let loaded: GldfProduct = GldfProduct::load_gldf("./tests/data/test.gldf").unwrap();
     println!("{:?}", loaded);
     // Display pretty printed XML
     let yaserde_cfg = yaserde::ser::Config {
         perform_indent: true,
         ..Default::default()
     };
-
     let x_serialized = yaserde::ser::to_string_with_config(&loaded, &yaserde_cfg).unwrap();
-
     println!("{}", x_serialized);
     let json_str = serde_json::to_string(&loaded).unwrap();
     println!("{}", json_str);
@@ -27,8 +21,19 @@ fn parsing_gldf_namespace() {
     let x_reserialized = yaserde::ser::to_string_with_config(&j_loaded, &yaserde_cfg).unwrap();
     println!("{}", x_reserialized);
     assert_eq!(x_serialized, x_reserialized);
-    // assert_eq!(loaded.tabpage[0].control.len(), 13);
-    // assert_eq!(loaded.tabpage[1].control.len(), 16);
-    // assert_eq!(loaded.tabpage[2].control.len(), 65);
-    // assert_eq!(loaded.tabpage[3].control.len(), 40);
+}
+
+#[test]
+fn test_gldf_product_impls() {
+    let loaded: GldfProduct = GldfProduct::load_gldf("./tests/data/test.gldf").unwrap();
+    println!("{:?}", loaded);
+    // Display pretty printed XML
+    let x_serialized = loaded.to_xml().unwrap();
+    println!("{}", x_serialized);
+    let json_str = loaded.to_json().unwrap();
+    println!("{}", json_str);
+    let j_loaded: GldfProduct = GldfProduct::from_json(&json_str).unwrap();
+    let x_reserialized =  j_loaded.to_xml().unwrap();
+    println!("{}", x_reserialized);
+    assert_eq!(x_serialized, x_reserialized);
 }
